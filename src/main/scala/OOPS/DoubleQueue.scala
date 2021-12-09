758a03c7029c4edf022752978ec6034b0e539157
Vaibhav Kumar <vaibhav.kumar@knoldus.com>
2021-12-09 12:06:59 +0530

    checked working

--- src/main/scala/OOPS/DoubleQueue.scala
+++ src/main/scala/OOPS/DoubleQueue.scala
@@ -1,5 +1,13 @@
 package OOPS
+import scala.collection.mutable.ListBuffer
 
-class DoubleQueue {
+
+//To insert double of an element
+class DoubleQueue(queue : ListBuffer[Int]) extends Queue {
+
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * 2)
+  }
 
 }
--- src/main/scala/OOPS/SquareQueue.scala
+++ src/main/scala/OOPS/SquareQueue.scala
@@ -1,5 +1,11 @@
 package OOPS
+import scala.collection.mutable.ListBuffer
 
-class SquareQueue {
+//To insert Square of an element
+class SquareQueue(queue: ListBuffer[Int]) extends Queue {
 
+  override def enqueue(elementToBePushed: Int): Unit = {
+
+    queue += (elementToBePushed * elementToBePushed)
+  }
 }
